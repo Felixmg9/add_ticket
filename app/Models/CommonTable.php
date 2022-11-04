9f1874baee6d089f22e7c013d46e5cfabb4f900d
Felixmg9 <116297843+Felixmg9@users.noreply.github.com>
2022-11-04 18:33:42 +0300

    Update CommonTable.php

--- app/Models/CommonTable.php
+++ app/Models/CommonTable.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+// protected $set_fields - set of required fields for save this table 
 
 abstract class CommonTable extends Model
 {
@@ -15,12 +16,13 @@ abstract class CommonTable extends Model
 		foreach($this->set_fields as $key)
 	    		$this->{$key} = $data[$key];
  		
-        	$this->save();
-		$table_name = strtolower(get_class($this));
-
+        $this->save();
+		
 		if (empty($this->id))
 			throw new Exception("no new record {$this->table}");
-		$data += [strtolower($this->table) . '_id' => $this->id];
-		return;
+		
+        $data += [strtolower($this->table) . '_id' => $this->id];
+		
+        return;
     	}
 }
